209e1d7e00a18199d15c60dc7558a123dd958ebb
SharadaGurrala <gurralasharada74@gmail.com>
2024-06-18 20:30:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function generateResume() {
+    const form = document.getElementById('resumeForm');
+    const preview = document.getElementById('resumePreview');
+
+    const name = form.name.value;
+    const contact = form.contact.value;
+    const summary = form.summary.value;
+    const skills = form.skills.value;
+    const education = form.education.value;
+    const experience = form.experience.value;
+    const projects = form.projects.value;
+    const certifications = form.certifications
+};
